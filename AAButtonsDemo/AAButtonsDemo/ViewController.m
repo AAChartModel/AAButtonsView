f58fd04303d2d8efe9d9f429569c08bdd8a43c19
李照闫 <2236368544@qq.com>
2017-08-07 17:19:29 +0800

    Change the property name

--- AAButtonsDemo/AAButtonsDemo/ViewController.m
+++ AAButtonsDemo/AAButtonsDemo/ViewController.m
@@ -21,9 +21,9 @@
     btnsView.selectedBtnDelegate = self;
             NSArray *tarr = @[@"AAChartKit",@"AAChartKit-Swift",@"AAButtonsView",@"XcodeHelp-CHINESE",@"AACategory",@"AAMapKit",@"AAChartKit-Slim",@"AATextWatchdog",@"社交恐惧",@"家庭矛盾",@"失恋",@"局势很简单",@"Word",@"美女",@"美女与野兽",@"体育",@"生化危机"];
                 NSArray *seletedArr = @[@"盗墓笔记",@"空空道人谈股市",@"叶文有话要说",@"相声",@"二货一箩筐",@"单田方",@"城市",@"美女",@"社交恐惧",@"家庭矛盾",@"失恋",@"局势很简单",@"Word",@"美女",@"美女与野兽",@"体育",@"生化危机"];
-    btnsView.layoutType = AAButtonsViewLayoutTypeOrderly;
-    btnsView.selectedBtnTitlesArr = seletedArr;
-    btnsView.normalBtnTitlesArr = tarr;
+    btnsView.layoutType = AAButtonsViewLayoutTypeStaggered;
+    btnsView.selectedBtnsTitleArr = seletedArr;
+    btnsView.btnsTitleArr = tarr;
     btnsView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
     btnsView.selectedBtnBlock = ^(UIButton *button) {
          NSLog(@"当前点击的按钮的标题是 %@",button.titleLabel.text);
